beea223ae71f7029a1075f306c294ba4185b08e0
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-14 13:28:51 -0300

    adicionando arquivos do desafio

---
+++ desafios infra/Terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.2.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.30.0"
+    }
+  }
+}
+provider "aws" {
+  region  = "us-east-1"
+  profile = "gasilva"
+}
+
