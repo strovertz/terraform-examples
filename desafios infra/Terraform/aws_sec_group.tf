e59d3220e7045000ab5bed728c04e5fd851fbb13
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-15 10:42:25 -0300

    atualizacao do ingress ssh

--- desafios infra/Terraform/aws_sec_group.tf
+++ desafios infra/Terraform/aws_sec_group.tf
@@ -20,21 +20,18 @@ resource "aws_security_group" "default" {
     ipv6_cidr_blocks = [ "::/0" ]
 
   }
+  #ssh ingress 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = var.tcp
+    cidr_blocks = [var.cidr_blocks_ssh]
+  }
   # Liberar todo o tráfego de saida
   egress {
     from_port   = 0
     to_port     = 65535
     protocol    = var.tcp
     cidr_blocks = [var.cidr_blocks]
-  }
-}
-
-resource "aws_security_group" "ssh_open" {
-  # Liberar a porta para acesso restrito via ssh
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = var.tcp
-    cidr_blocks = [var.cidr_blocks_ssh]
   }
 }
