57f157ebaae8e4073f70b73a75937c9436eb7536
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-25 14:58:40 -0300

    namespace + helm + atualização de vars

--- desafios infra/desafio4/variables.tf
+++ desafios infra/desafio4/variables.tf
@@ -100,3 +100,56 @@ variable "PUBLIC_KEY_PATH" {
 variable "EC2_USER" {
   default = "ubuntu"
 }
+
+variable "create_namespace" {
+  type        = bool
+  default     = true
+  description = "Whether to create Kubernetes namespace with name defined by `namespace`."
+}
+
+variable "namespace" {
+  type        = string
+  default     = "kube-system"
+  description = "Kubernetes namespace to deploy Cluster Autoscaler Helm chart."
+}
+
+variable "mod_dependency" {
+  default     = null
+  description = "Dependence variable binds all AWS resources allocated by this module, dependent modules reference this variable."
+}
+
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Variable indicating whether deployment is enabled."
+}
+
+variable "helm_chart_name" {
+  type        = string
+  default     = "cluster-autoscaler"
+  description = "Cluster Autoscaler Helm chart name to be installed"
+}
+
+variable "helm_chart_release_name" {
+  type        = string
+  default     = "cluster-autoscaler"
+  description = "Helm release name"
+}
+
+variable "fullname_override" {
+  type        = string
+  default     = "aws-cluster-autoscaler"
+  description = "Helm fullnameOverride"
+}
+
+variable "helm_chart_version" {
+  type        = string
+  default     = "9.9.2"
+  description = "Cluster Autoscaler Helm chart version."
+}
+
+variable "helm_chart_repo" {
+  type        = string
+  default     = "https://kubernetes.github.io/autoscaler"
+  description = "Cluster Autoscaler repository name."
+}
