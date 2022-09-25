bb3b7f1b5e5ba3fdcbd302a0cf238d623315e6e0
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-25 11:24:16 -0300

    atualizando eks e subnets

--- desafios infra/desafio2/ekscluster.tf
+++ desafios infra/desafio2/ekscluster.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.eks-iam.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.prod-subnet-public-1.id]
+    subnet_ids = [aws_subnet.prod-subnet-public-1.id, aws_subnet.prod-subnet-public-2.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
