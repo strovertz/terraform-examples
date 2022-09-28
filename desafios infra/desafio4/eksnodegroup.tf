46f50a2a8753e4b520806cd6bd974c890cadebf1
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-27 21:51:17 -0300

    adicionando lt e asg

--- desafios infra/desafio4/eksnodegroup.tf
+++ desafios infra/desafio4/eksnodegroup.tf
@@ -22,3 +22,20 @@ resource "aws_eks_node_group" "nginx" {
     aws_iam_role_policy_attachment.node-group-iam-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+resource "aws_autoscaling_group_tag" "example" {
+  for_each = toset(
+    [for asg in flatten(
+      [for resources in aws_eks_node_group.nginx.resources : resources.autoscaling_groups]
+    ) : asg.name]
+  )
+
+  autoscaling_group_name = aws_autoscaling_group.bar.name
+
+  tag {
+    key   = "k8s.io/cluster-autoscaler/node-template/label/eks.amazonaws.com/capacityType"
+    value = "SPOT"
+
+    propagate_at_launch = false
+  }
+}
